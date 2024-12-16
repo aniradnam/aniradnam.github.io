b59a2ba415c1a931369133abfa3bda6a109d6b72
Marcos Laina <marcos.laina@loudintelligence.com>
2024-12-16 16:21:34 +0100

    pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,22 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
+const SCRIPT = process.env.npm_lifecycle_script || '';
+
+const isBuild = SCRIPT.includes('build');
+
+const LOCALHOST_URL = 'http://localhost:4321';
+
+const LIVE_URL = 'https://aniradnam.github.io';
+
+let BASE_URL = LOCALHOST_URL;
+
+if (isBuild) {
+  BASE_URL = LIVE_URL;
+}
+
+
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://aniradnam.github.io',
-  base: '/aniradnam',
+  site: 'https://aniradnam.github.io'
 });
