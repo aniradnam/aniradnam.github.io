3e8b83adbbad4581e4ccf1df5d85e38ec660dca4
Marcos Laina <marcos.laina@loudintelligence.com>
2024-12-17 19:17:40 +0100

    notion

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
 const SCRIPT = process.env.npm_lifecycle_script || '';
-
 const isBuild = SCRIPT.includes('build');
-
 const LOCALHOST_URL = 'http://localhost:4321';
-
 const LIVE_URL = 'https://aniradnam.github.io';
 
 let BASE_URL = LOCALHOST_URL;
@@ -19,6 +16,11 @@ if (isBuild) {
 
 
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), react()],
+  vite: {
+    define: {
+      'import.meta.env.NOTION_API_KEY': JSON.stringify(process.env.NOTION_API_KEY),
+    },
+  },
   site: 'https://aniradnam.github.io'
 });
