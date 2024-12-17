332924219e49ddade35749cc0ee79ff164dcfe52
Rolnyr <105305090+Rolnyr@users.noreply.github.com>
2024-12-17 23:12:16 +0100

    Welcome to the jungle

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,25 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		screens: {
+			sm: '480px',
+			md: '768px',
+			lg: '976px',
+			xl: '1440px',
+		},
+		
+		extend: {
+			colors: {
+				primary: {
+					DEFAULT: '#252422',
+					
+				},
+				secondary: {
+					DEFAULT: '#eb5e28',
+					
+				}
+		},
+				},
 	},
 	plugins: [],
 }
