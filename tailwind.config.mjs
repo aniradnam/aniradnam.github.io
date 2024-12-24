195725bcba25340efe4cf55b66bfc8bf8f51e1c0
Rolnyr <105305090+Rolnyr@users.noreply.github.com>
2024-12-24 13:38:29 +0100

    24/12/2024-01

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,13 +2,7 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		screens: {
-			sm: '480px',
-			md: '768px',
-			lg: '976px',
-			xl: '1440px',
-		},
-		
+	
 		extend: {
 			colors: {
 				primary: {
